af9ab07e4b79fbd5de5e2efa82cc81e58a58d0b0
uday_ahuja <ahujaud@gmail.com>
2019-02-26 20:42:36 +0530

    Removed NgRX and Routing projects

--- NgRx/src/app/Demo1/app-demo1.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-demo1',
-  templateUrl: './app-demo1.component.html',
-  styleUrls: ['./app-demo1.component.css']
-})
-export class AppDemo1Component { }
